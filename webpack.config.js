48ad09ecb08264edd69e0a5ef7f28f1ddac4e4a4
Sanjit Kurar <sanjit@binmile.com>
2022-04-13 17:09:56 +0530

    Webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ const path = require("path ");
 
 module.exports = {
   entry: "/src/index.js",
-  output: { path: path.resolve(dirname, "dist") },
+  output: { path: path.resolve(__dirname, "dist") },
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
